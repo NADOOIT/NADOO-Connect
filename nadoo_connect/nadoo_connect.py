858e985a782ff09b7f5c59dff00fefe473186e6b
Christoph Backhaus <christoph.backhaus@nadooit.de>
2023-12-30 16:41:15 +0100

    create exectution done

--- nadoo_connect/nadoo_connect.py
+++ nadoo_connect/nadoo_connect.py
@@ -17,6 +17,7 @@ config = None
 idle_time = 120  # Idle time in seconds before shutting down the sender loop
 max_wait_time = 120  # Maximum wait time in seconds between retries
 sender_task = None  # Global variable to keep track of the sender task
+email_size_limit = 72 * 1024  # Email size limit in bytes (72 KB)
 
 
 def setup_directories():
@@ -26,6 +27,12 @@ def setup_directories():
 
 def load_or_request_config():
     load_dotenv()
+    config = get_config_from_env_or_prompt()
+    save_missing_config_to_env(config)
+    return config
+
+
+def get_config_from_env_or_prompt():
     required_vars = [
         "SMTP_SERVER",
         "SMTP_PORT",
@@ -34,22 +41,29 @@ def load_or_request_config():
         "DESTINATION_EMAIL",
     ]
     config = {var: os.getenv(var) for var in required_vars}
-
     missing_configs = {var for var in required_vars if not config[var]}
     if missing_configs:
-        root = Tk()
-        root.withdraw()
-        for var in missing_configs:
-            config[var] = simpledialog.askstring(
-                "Configuration", f"Enter your {var.lower()}:"
-            )
-        root.destroy()
-
-        with open(".env", "a") as env_file:
-            for var in missing_configs:
-                env_file.write(f"{var}={config[var]}\n")
-
+        config.update(request_missing_config(missing_configs))
     return config
+
+
+def request_missing_config(missing_configs):
+    root = Tk()
+    root.withdraw()
+    config_updates = {}
+    for var in missing_configs:
+        config_updates[var] = simpledialog.askstring(
+            "Configuration", f"Enter your {var.lower()}:"
+        )
+    root.destroy()
+    return config_updates
+
+
+def save_missing_config_to_env(config):
+    with open(".env", "a") as env_file:
+        for var, value in config.items():
+            if value is not None:
+                env_file.write(f"{var}={value}\n")
 
 
 async def send_async_email(
@@ -77,7 +91,7 @@ def record_execution_in_db(execution_uuid, customer_program_uuid, is_sent):
     cursor = conn.cursor()
     cursor.execute(
         """CREATE TABLE IF NOT EXISTS execution_records 
-                      (execution_uuid TEXT, customer_program_uuid TEXT, is_sent BOOLEAN)"""
+           (execution_uuid TEXT, customer_program_uuid TEXT, is_sent BOOLEAN)"""
     )
     cursor.execute(
         "INSERT INTO execution_records VALUES (?, ?, ?)",
@@ -93,26 +107,45 @@ def create_execution(customer_program_uuid):
         config = load_or_request_config()
 
     setup_directories()
+    execution_data = get_execution_data(customer_program_uuid)
+    save_execution_data(execution_data)
+
+    start_sender_loop_if_not_running()
+
+
+def get_execution_data(customer_program_uuid):
     execution_uuid = str(uuid.uuid4())
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-    execution_data = {
+    return {
         "execution_uuid": execution_uuid,
         "customer_program_uuid": customer_program_uuid,
         "timestamp": timestamp,
     }
-    filepath = os.path.join(executions_dir, f"{execution_uuid}.json")
+
+
+def save_execution_data(execution_data):
+    filepath = os.path.join(executions_dir, f"{execution_data['execution_uuid']}.json")
     with open(filepath, "w") as file:
         json.dump(execution_data, file)
 
-    # Manage the sender loop as an asyncio task
+
+def start_sender_loop_if_not_running():
+    global sender_task
     if sender_task is None or sender_task.done():
         try:
             with portalocker.Lock(lockfile_path, mode="w", timeout=1):
-                loop = asyncio.get_event_loop()
+                try:
+                    loop = asyncio.get_running_loop()
+                except RuntimeError:
+                    # Create a new event loop if there isn't a running one
+                    loop = asyncio.new_event_loop()
+                    asyncio.set_event_loop(loop)
+
                 if loop.is_running():
                     sender_task = loop.create_task(sender_loop())
                 else:
-                    asyncio.run(sender_loop())
+                    sender_task = loop.create_task(sender_loop())
+                    loop.run_until_complete(sender_task)
         except portalocker.exceptions.LockException:
             pass  # Another process is already running the sender loop
 
@@ -122,67 +155,68 @@ async def sender_loop():
     last_execution_time = time.time()
     while True:
         try:
-            executions_exist = False
-            print(f"Checking for executions. Current wait time: {wait_time} seconds.")
-            for filename in os.listdir(executions_dir):
-                filepath = os.path.join(executions_dir, filename)
-                if filename.endswith(".json"):
-                    executions_exist = True
-                    print(f"Processing file: {filename}")
-                    with open(filepath, "r") as file:
-                        execution_data = json.load(file)
-
-                    email_sent = await send_async_email(
-                        "Execution Created",
-                        json.dumps(execution_data),
-                        config["DESTINATION_EMAIL"],
-                        config["SMTP_SERVER"],
-                        int(config["SMTP_PORT"]),
-                        config["EMAIL"],
-                        config["PASSWORD"],
-                    )
-                    if email_sent:
-                        os.remove(filepath)
-                        record_execution_in_db(
-                            execution_data["execution_uuid"],
-                            execution_data["customer_program_uuid"],
-                            True,
-                        )
-                        last_execution_time = time.time()
-                        wait_time = 10  # Reset wait time after successful send
-                        print(f"Email sent, resetting wait time.")
-                    else:
-                        wait_time = min(
-                            wait_time * 2, max_wait_time
-                        )  # Exponential backoff
-                        print(
-                            f"Email sending failed, increasing wait time to {wait_time} seconds."
-                        )
-
-            if not executions_exist:
-                remaining_idle_time = idle_time - (time.time() - last_execution_time)
-                print(
-                    f"No executions found. Remaining idle time before shutdown: {remaining_idle_time:.2f} seconds."
-                )
-                if remaining_idle_time <= 0:
-                    print("Idle time exceeded, shutting down sender loop.")
+            execution_files = [
+                f for f in os.listdir(executions_dir) if f.endswith(".json")
+            ]
+            if not execution_files:
+                if time.time() - last_execution_time > idle_time:
                     break
-            else:
-                print("Executions exist, resetting idle timer.")
-                last_execution_time = time.time()
+                await asyncio.sleep(wait_time)
+                continue
 
-            await asyncio.sleep(wait_time)
+            batched_execution_data = []
+            for filename in execution_files:
+                filepath = os.path.join(executions_dir, filename)
+                with open(filepath, "r") as file:
+                    execution_data = json.load(file)
+                    batched_execution_data.append(execution_data)
+
+                # Check if the combined size of the batched data is approaching the email size limit
+                # (You will need to define 'email_size_limit')
+                if calculate_size(batched_execution_data) > email_size_limit:
+                    break
+
+            email_content = json.dumps(batched_execution_data)
+            email_sent = await send_async_email(
+                "Batched Executions",
+                email_content,
+                config["DESTINATION_EMAIL"],
+                config["SMTP_SERVER"],
+                int(config["SMTP_PORT"]),
+                config["EMAIL"],
+                config["PASSWORD"],
+            )
+
+            if email_sent:
+                for data in batched_execution_data:
+                    os.remove(
+                        os.path.join(executions_dir, f"{data['execution_uuid']}.json")
+                    )
+                    record_execution_in_db(
+                        data["execution_uuid"],
+                        data["customer_program_uuid"],
+                        True,
+                    )
+                last_execution_time = time.time()
+                wait_time = 10
+            else:
+                wait_time = min(wait_time * 2, max_wait_time)
+
         except asyncio.CancelledError:
             break
         except Exception as e:
             print(f"Error in sender loop: {e}")
 
 
+def calculate_size(data):
+    return len(json.dumps(data))  # Returns the size of the data in bytes
+
+
 def main():
     global config
     config = load_or_request_config()
     customer_program_uuid = "specific-uuid-from-database"
-    for i in range(10):
+    for i in range(100):
         create_execution(customer_program_uuid)
 
 
